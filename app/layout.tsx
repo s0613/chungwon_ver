bb01be1c12b99579f61fe9dd71863c1c80b47647
SongSeungJu <wkwjsrj5079@gmail.com>
2025-12-11 12:39:10 +0900

    Add Google Analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -28,7 +29,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-108G386MX0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-108G386MX0');
+          `}
+        </Script>
+      </body>
     </html>
   )
 }
